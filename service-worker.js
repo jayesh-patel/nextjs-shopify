26c672261c7a28a7eb3a2b57583a62f57bb8942c
Jayesh Patel <jayesh.patel@aureatelabs.com>
2022-11-15 15:15:42 +0530

    Modified prefetch constant.

--- service-worker.js
+++ service-worker.js
@@ -9,12 +9,17 @@ clientsClaim();
 
 // must include following lines when using inject manifest module from workbox
 // https://developers.google.com/web/tools/workbox/guides/precache-files/workbox-build#add_an_injection_point
-const WB_MANIFEST = self.__WB_MANIFEST;
+//const WB_MANIFEST = self.__WB_MANIFEST;
+const WB_MANIFEST = [];
 // Precache fallback route and image
 WB_MANIFEST.push({
   url: '/fallback',
   revision: '1234567890',
 });
+WB_MANIFEST.push({
+    url: '/',
+    revision: '1234567890',
+  });
 precacheAndRoute(WB_MANIFEST);
 
 cleanupOutdatedCaches();
